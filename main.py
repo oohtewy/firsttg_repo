e8455226272b443a56aec49327e7bbd40ac8da8d
oohtewy <katanikitin@gmail.com>
2023-08-11 19:01:33 +0500

    third_commit

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from telegram import *
 from telegram.ext import *
 
 TOKEN = '6634395327:AAEs2uvU-TvhbtadllvWDHucpHZesGY9HK0'
+
+RANDOM_IMAGE = 'Random image'
+GET_MP3 = 'Get MP3'
+RANDOM_IMAGE_URL = 'https://picsum.photos/1200/'
+
+global IMAGE_COUNTER
+IMAGE_COUNTER = 0 
+
+print('Runnig up to the bot...')
+
