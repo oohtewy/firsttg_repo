c297e950c2ccb79b1e2aff3f6c3aae658e626d38
oohtewy <katanikitin@gmail.com>
2023-08-11 19:45:13 +0500

    last

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("help", help))
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("buttons", get_buttons))
-dispatcher.add_handler(CommandHandler(Filters.text, message_handler))
+dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
 updater.start_polling()
 updater.idle()
